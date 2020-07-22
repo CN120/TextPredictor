42b4a6732b11f108ca911f7734eec3cc931d0f6c
Chris Nelson <christopher.nelson@usa.com>
2020-07-22 11:58:10 -0700

    changed from delete to reoving gray tags

--- text_predictor.py
+++ text_predictor.py
@@ -4,12 +4,15 @@ from tkinter import ttk as ttk
 from time import sleep
 from tkinter import font
 
-
+#common words
 com_words = np.loadtxt("google-10000-english-usa-no-swears.txt", dtype=str, delimiter='\n')
-print(com_words)
+rel_words = com_words 
+#relavant words
+
 cur_word = ''
 
 root = tk.Tk()
+root.unbind_all("<Tab>")
 root.title("Text Predictor")
 appFont = font.Font(family='menlo', size=12)
 
@@ -17,29 +20,48 @@ txt = tk.Text(root,highlightthickness=0 , font = appFont)
 txt.pack(fill=tk.BOTH,side='left', expand=1)
 txt.tag_configure('gray', foreground='#bfbfbf')
 
-def key(event):
-    global cur_word
-    txt.delete('insert',tk.END)
+def alpha(event):
+    global cur_word, rel_words
+    print(event)
     print("pressed", repr(event.char))
     typed_char = event.char
     if typed_char.isalnum():
+        txt.delete('insert',tk.END)
         cur_word+=typed_char
         cur_index =  txt.index('insert')
-        word_bool_arr = np.char.startswith(com_words,cur_word)
-        found_list = com_words[word_bool_arr]
+        rel_words = rel_words[np.char.startswith(rel_words,cur_word)]
         try:
-            txt.insert(tk.END,found_list[0][len(cur_word):],'gray')
+            txt.insert(tk.END,rel_words[0][len(cur_word):],'gray')
         except:
             pass
         print(txt.index('insert'))
         txt.mark_set("insert", cur_index)
         print(cur_word)
-    else:
+    elif typed_char == '\t':
+        txt.tag_remove("gray",  "1.0", tk.END)
+        txt.mark_set("insert", tk.END)
+        txt.insert(tk.END,' ')
         cur_word = ''
+        rel_words = com_words
+    elif typed_char == '\x7f':
+        cur_word = cur_word[:-1]
+    else:
+        txt.delete('insert',tk.END)
+        cur_word = ''
+        rel_words = com_words
+
+def tabdown(event):
+    return ("break")
+    # global cur_word, rel_words
+    # txt.delete('insert',tk.END)
+    # root.update()
+    # txt.insert(tk.END,rel_words[0][len(cur_word):])
+    # rel_words = com_words
+    # print("yes")
 
 
-
-txt.bind("<KeyRelease>", key)
+txt.bind("<KeyRelease>", alpha)
+txt.bind("<Tab>", tabdown)
 # # apply the tag "red" 
 # txt.highlight_pattern("word", "gray")
 scrollbar = ttk.Scrollbar(root)
