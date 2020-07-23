787d8dcc03988e729c2193ced22e2601f8d1a3fe
Chris Nelson <christopher.nelson@usa.com>
2020-07-22 18:58:57 -0700

    tweaks/fixes

--- text_predictor.py
+++ text_predictor.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tkinter as tk
 from tkinter import ttk as ttk
-from time import sleep
+# from time import sleep
 from tkinter import font
 
 #common words
@@ -19,23 +19,23 @@ menubar = tk.Menu(root)
 appFont = font.Font(family='menlo', size=12)
 
 
-
-
 txt = tk.Text(root,highlightthickness=0 , font = appFont, wrap=tk.WORD)
 txt.pack(fill=tk.BOTH,side='left', expand=1)
 txt.tag_configure('prediction', foreground='#bfbfbf')
 
+# class TextPredictor:
+#     def __init__(self, parent, *args, **kwargs):
 
 def alpha(event):
     global cur_word, rel_words
     print(event)
     # print("pressed", repr(event.char))
-    typed_char = event.char
-    if typed_char.isalnum():
+    typed_key = event.char
+    if typed_key.isalnum():
         #delete previous guess
         txt.delete('insert',tk.END)
         #add le
-        cur_word+=typed_char
+        cur_word+=typed_key.lower()
         cur_index =  txt.index('insert')
         rel_words = rel_words[np.char.startswith(rel_words,cur_word)]
         try:
@@ -50,40 +50,54 @@ def alpha(event):
         print(txt.index('insert'))
         txt.mark_set("insert", cur_index)
         print("word:",cur_word)
-    elif typed_char == '\t':
+    elif typed_key == '\t':
         txt.tag_remove("prediction",  "1.0", tk.END)
         txt.mark_set("insert", tk.END)
-        txt.insert(tk.END,' ')
+        if len(cur_word)>0:
+            txt.insert(tk.END,' ')
+        else:
+            txt.insert(tk.END,'\t')
         cur_word = ''
         rel_words = com_words
-    elif typed_char == '\x7f':  #backspace
+    elif event.keysym == 'BackSpace':  #backspace
         # if(event.state=='8'):
         # print(event.state)
         txt.delete('insert',tk.END)
         cur_index =  txt.index('insert') 
            
         if txt.get('insert-1c',tk.INSERT).isalnum():
-            cur_word = txt.get('1.0',tk.END).split()[-1]
+            cur_word = txt.get('1.0',tk.END).split()[-1].lower()
             rel_words = com_words[np.char.startswith(com_words,cur_word)]
             try:
                 txt.insert(tk.END,rel_words[0][len(cur_word):],'prediction')
             except:
                 pass
         else:
-            cur_word=''
+            cur_word = ''
         txt.mark_set("insert", cur_index)
         print("word:",cur_word)
-    else:
+    elif event.keysym == 'space':
         txt.delete('insert',tk.END)
         cur_word = ''
         rel_words = com_words
+    # else:
+    #     txt.delete('insert',tk.END)
+    #     cur_word = ''
+    #     rel_words = com_words
+
+
 
 def tabdown(event):
     return ("break")
 
+def key(event):
+    alpha(event)
+    return
 
+# txt.bind("<Key>", key)
 txt.bind("<KeyRelease>", alpha)
 txt.bind("<Tab>", tabdown)
+# txt.bind("<Meta_L-s>", lambda: print("SAVE"))
 
 scrollbar = ttk.Scrollbar(root)
 scrollbar.pack(side='right',fill='y')
